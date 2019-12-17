e6f2f9898bf370aefe1cde5229613191902a4ba6
Gabriel Silva <martinsgpgabriel@gmail.com>
2019-12-17 16:23:17 -0300

    Inserindo busca, e modificações para adequar o modeladmin

--- projeto/app/models_admin/category_admin.py
+++ projeto/app/models_admin/category_admin.py
@@ -3,4 +3,6 @@ from ..forms import CategoryForm
 
 
 class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'created_at', 'updated_at')
+    search_fields = ('name',)
     form = CategoryForm
--- projeto/app/models_admin/post_admin.py
+++ projeto/app/models_admin/post_admin.py
@@ -3,4 +3,11 @@ from ..forms import PostForm
 
 
 class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'publish', 'user', 'created_at', 'updated_at']
+    search_fields = ('title',)
+    list_filter = ('publish', 'user')
     form = PostForm
+
+    def save_model(self, request, obj, form, change):
+        obj.user = request.user
+        obj.save()
